2413589150c5fc55665bc16b0b9c08afe4ee5e52
Shaikh Imran <shaikhimran115@gmail.com>
2024-06-14 20:12:14 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,8 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8080/products";
+            const url = `${process.env.REACT_APP_API_URL}/products`;
+            // const url = "http://localhost:8080/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
